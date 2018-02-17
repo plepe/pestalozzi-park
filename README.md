f1c2dac365563ef432bf453a81bdca7cf2d818e4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-02-17 09:32:15 +0100

    Improve documentation

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
+Creates a heatmap of the addresses of the [petition for the Pestalozzi Park](http://www.rettetdenpestalozzipark.at/) in Leoben Donawitz, Austria.
+
 # INSTALL
 ```sh
 git clone https://github.com/plepe/pestalozzi-park.git
 cd pestalozzi-park
 npm install
-./decode.sh
+# update file list.txt
+./decode.sh > result.csv
 ```
--- decode.sh
+++ decode.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
+/* dependencies */
 var fs = require('fs')
 var request = require('request')
 var async = require('async')
 
+/* read file 'list.txt' and split into lines */
 var fileData = fs.readFileSync('list.txt').toString()
 fileData = fileData.split(/\n/)
 
+/* initialize variables:
+ * - hash map 'uniqueEntries' will contain each unique address from
+ *   list.txt as key and count of appearances as value, e.g.
+ *   { "address1": 5, "address2": 1 }
+ */
 var uniqueEntries = {}
+/* - hash map 'results' will contain each successfully resolved address as key and the (first) result from Nominatim as value, e.g.
+ *   { "address1": { "lat": 48.1989256, "lon": 16.3698784, ... }, ... }
+ */
 var results = {}
 
+/* create the uniqueEntries hash map */
 fileData.forEach(function (entry) {
   if (entry in uniqueEntries) {
     uniqueEntries[entry]++
@@ -17,7 +28,12 @@ fileData.forEach(function (entry) {
   }
 })
 
-async.eachLimit(Object.keys(uniqueEntries), 5, function (entry, callback) {
+/* for each address (but not more than 5 simultaneously -> eachLimit waits
+ * until each function calls the 'callback' method) - at the end call the
+ * final function ... */
+async.eachLimit(Object.keys(uniqueEntries), 5,
+function (entry, callback) {
+  /* prepare options to 'request' */
   var options = {
     url: 'https://nominatim.openstreetmap.org/search/' + encodeURIComponent(entry) + '?format=json',
     headers: {
@@ -25,8 +41,10 @@ async.eachLimit(Object.keys(uniqueEntries), 5, function (entry, callback) {
     }
   }
 
+  /* request result from Nominatim API */
   request(options,
     function (error, response, body) {
+      /* try to parse answer - on error case, print error to stderr */
       try {
         body = JSON.parse(body)
       } catch(err) {
@@ -35,20 +53,26 @@ async.eachLimit(Object.keys(uniqueEntries), 5, function (entry, callback) {
          return
       }
 
+      /* if result is empty, print error to stderr */
       if (!body.length) {
         console.error('No results for \"' + entry + '\" returned!')
         callback()
         return
       }
 
+      /* success! -> save result in 'results' hash map */
       results[entry] = body[0]
 
       callback()
     }
   )
-}, function () {
-  // function will be called when all entries have been requested
+},
+/* will be called when all entries have been resolved */
+function () {
+  /* for each entry in the fileData array ... */
   fileData.forEach(function (entry) {
+
+    /* print the result, if available */
     var result = results[entry]
     if (result) {
       console.log(result.lat + ', ' + result.lon)
